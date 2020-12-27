f97cc0da5503acf54f7321f5b54bd07761718b80
JacobCai <1499164758@qq.com>
2020-12-27 21:20:16 +0800

    Add project files.

---
+++ Blazor.Shared/Dtos/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Shared.Dtos
+{
+    public enum Gender
+    {
+        NotSpecified,
+        Female,
+        Male
+    }
+}
---
+++ Blazor.Shared/Dtos/HealthCondition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Shared.Dtos
+{
+    public enum HealthCondition
+    {
+        Healthy,
+        Cold,
+        Fever,
+        Other
+    }
+}
